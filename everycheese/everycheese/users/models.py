b0f6c3515c0aaec02e84fb12a35ca9d1b36aaa87
Richard Viens <richard.viens444@outlook.com>
2023-09-25 16:48:01 -0400

    First commit

---
+++ everycheese/everycheese/users/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    
+    
+    bio = models.TextField("Bio", blank=True)
+  
+    name = models.CharField(
+        _("Name of User"), blank=True, max_length=255
+    )
+
+    def get_absolute_url(self):
+        return reverse(
+            "users:detail", kwargs={"username": self.username}
+        )
